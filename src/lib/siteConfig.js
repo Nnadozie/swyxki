9d94debddfe1974b79c99f0affd69e6a7cd1f106
Nnadozie Okeke <n.e.okeke@outlook.com>
2024-11-20 03:03:34 -0700

    add og image

--- src/lib/siteConfig.js
+++ src/lib/siteConfig.js
@@ -1,15 +1,14 @@
-export const SITE_URL = 'https://nnadozie.com';
+export const SITE_URL = 'https://dozie.dev';
 export const APPROVED_POSTERS_GH_USERNAME = ['Nnadozie'];
 export const GH_USER_REPO = 'Nnadozie/swyxkit';
 export const REPO_URL = 'https://github.com/' + GH_USER_REPO;
 export const SITE_TITLE = 'Nnadozie Okeke';
 export const SITE_DESCRIPTION = "Software and AI Engineer building SaaS products";
-export const DEFAULT_OG_IMAGE = '';
+export const DEFAULT_OG_IMAGE = 'https://avatars.githubusercontent.com/u/15310842?v=4';
 export const MY_TWITTER_HANDLE = 'dozieokk';
 export const MY_YOUTUBE = 'https://youtube.com/doziedev';
 export const POST_CATEGORIES = ['Essay', 'Note'];
 export const GH_PUBLISHED_TAGS = ['Published'];
-export const REPO_OWNER = 'Nnadozie Okeke';
 
 // dont forget process.env.GH_TOKEN
 // if supplied, raises rate limit from 60 to 5000
